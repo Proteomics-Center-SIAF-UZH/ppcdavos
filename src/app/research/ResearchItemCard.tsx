3db27d4b0f80769a06ca46fb0000efe4c45107f3
XiaoSunXS <sunxiaolive2016@outlook.com>
2023-07-23 23:16:30 +0200

    fix classname

--- src/app/research/ResearchItemCard.tsx
+++ src/app/research/ResearchItemCard.tsx
@@ -5,12 +5,12 @@ import { truncateString } from "../helper/trunctuateText";
 import { publications } from "./data";
 
 const IconUp = () => 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
     </svg>
 
 const IconDown = () => 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
     </svg>
 
