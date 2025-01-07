d0de6bd74958c7d817bd52cbce6088877d2d1a5a
Xiao Sun <xiaosun@Xiaos-MacBook-Air.local>
2025-01-07 22:39:31 +0100

    UI: improve navigation bar

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 const LinkItem = ({ name, href }: { name: string; href: string }) => (
   <a
     href={href}
-    className="hover:bg-sky-50 hover:rounded-md hover:text-sky-950 transition-all px-2 py-1"
+    className="hover:bg-sky-50 hover:rounded-md hover:text-sky-950 transition-all px-2 inline-flex items-center"
   >
     {name}
   </a>
@@ -9,25 +9,25 @@ const LinkItem = ({ name, href }: { name: string; href: string }) => (
 
 const Navigation = () => {
   return (
-    <div className="flex flex-col sm:flex-row sm:flex-nowrap justify-between px-16 py-8 bg-sky-950 text-white">
+    <div className="flex flex-col sm:flex-row sm:flex-nowrap justify-between items-end px-16 py-8 bg-sky-950 text-white">
       <a href="/" className="flex items-center">
-        <h1 className="text-xl font-bold my-1 flex items-center">
+        <h1 className="text-xl font-bold flex items-end">
           <img
             src={'/logos/White bg + logo 5k.png'}
-            alt={'Precision Proteomic Center Davos logo'}
-            className="w-10 h-auto%"
+            alt="Precision Proteomic Center Davos logo"
+            className="w-14 h-auto mb-1"
           />
           <p className="px-2">Precision Proteomic Center Davos</p>
         </h1>
       </a>
-      <div className="flex flex-col sm:flex-row sm:flex-nowrap">
+      <div className="flex flex-col sm:flex-row sm:flex-nowrap items-end">
+        <LinkItem href="/aboutUs" name="About us" />
         <LinkItem href="/team" name="Team" />
         <LinkItem href="/publications" name="Publications" />
         <LinkItem href="/infrastructure" name="Infrastructure" />
         <LinkItem href="/openPositions" name="Open Positions" />
       </div>
     </div>
-
   );
 };
 
