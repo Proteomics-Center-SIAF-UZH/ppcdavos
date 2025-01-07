0a6031bdbdaf5db7830239869a941e0c1480ce77
Xiao Sun <xiaosun@Xiaos-MacBook-Air.local>
2025-01-07 22:14:36 +0100

    feat: add logo files and add to the pagetitle

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -10,11 +10,16 @@ const LinkItem = ({ name, href }: { name: string; href: string }) => (
 const Navigation = () => {
   return (
     <div className="flex flex-col sm:flex-row sm:flex-nowrap justify-between px-16 py-8 bg-sky-950 text-white">
-      <div>
-        <a href="/">
-          <h1 className="text-xl font-bold px-2 my-1">Precision Proteomic Center Davos</h1>
-        </a>
-      </div>
+      <a href="/" className="flex items-center">
+        <h1 className="text-xl font-bold my-1 flex items-center">
+          <img
+            src={'logos/White bg + logo 5k.png'}
+            alt={'Precision Proteomic Center Davos logo'}
+            className="w-10 h-auto%"
+          />
+          <p className="px-2">Precision Proteomic Center Davos</p>
+        </h1>
+      </a>
       <div className="flex flex-col sm:flex-row sm:flex-nowrap">
         <LinkItem href="/team" name="Team" />
         <LinkItem href="/publications" name="Publications" />
